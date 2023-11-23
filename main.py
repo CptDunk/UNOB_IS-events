8af7d7d3734f872289379f6a6fc6c5f32456dfbf
CptDunk <vojtech.martinat@gmail.com>
2023-11-23 14:56:30 +0100

    Changed conenction string, added ICAL fastAPI endpoint

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ import typing
 import asyncio
 
 from fastapi import FastAPI
+
+from icalendar import Calendar, Event
+
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import PlainTextResponse
+
+
+
+
+
 import strawberry
 from strawberry.fastapi import GraphQLRouter
 
@@ -107,6 +117,48 @@ app = FastAPI()
 app.mount("/gql", graphql_app)
 
 
+
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import PlainTextResponse
+
+app = FastAPI()
+
+@app.post("/ical")
+async def create_ical():
+    try:
+        # Set the iCal data as a string
+        ical_data = """
+        BEGIN:VCALENDAR
+        VERSION:2.0
+        PRODID:-//hacksw/handcal//NONSGML v1.0//EN
+        BEGIN:VEVENT
+        UID:uid1@example.com
+        DTSTAMP:19970714T170000Z
+        ORGANIZER;CN=<link>John Doe</link>:MAILTO:john.doe@example.com
+        DTSTART:19970714T170000Z
+        DTEND:19970715T040000Z
+        SUMMARY:Bastille Day Party
+        GEO:48.85299;2.36885
+        END:VEVENT
+        END:VCALENDAR
+        """
+
+        # Set the appropriate response headers
+        headers = {
+            "Content-Disposition": "attachment; filename=calendar.ics",
+            "Content-Type": "text/calendar",
+        }
+
+        # Return the iCal data as the response
+        return PlainTextResponse(ical_data, headers=headers)
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@app.get("/ical")
+async def get_ical():
+    ical_data = """BEGIN:VCALENDAR\r\nVERSION:2.0\r\nPRODID:-//Example Corp//Calendar Application//EN\r\nBEGIN:VEVENT\r\nUID:9876543210\r\nDTSTAMP:20230215T150000Z\r\nDTSTART:20231224T180000Z\r\nDTEND:20231224T200000Z\r\nSUMMARY:Christmas Eve Celebration\r\nDESCRIPTION:Join us for a festive Christmas Eve celebration filled with joy and merriment.\r\nLOCATION:Your Home\r\nEND:VEVENT\r\nEND:VCALENDAR"""
+    return PlainTextResponse(ical_data, media_type="text/calendar")
 @app.on_event("startup")
 async def startup_event():
     initizalizedEngine = await RunOnceAndReturnSessionMaker()
@@ -115,9 +167,10 @@ async def startup_event():
 
 print("All initialization is done")
 
-# @app.get('/hello')
-# def hello():
-#    return {'hello': 'world'}
+@app.get('/hello')
+def hello():
+    return {'ICAL': 'FORMAT',
+            'BEGIN': 'VCALENDAR'}
 
 ###########################################################################################################################
 #
